52eabef5f0a748c53bbe6e35af87ca56a3379268
Victor Skits <vityok.nice@gmail.com>
2024-06-24 16:54:41 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,22 @@
+"use strict";
+const targetHeadings = document.querySelectorAll("th");
+for(let i = 0; i < targetHeadings.length; i++)targetHeadings[i].addEventListener("click", ()=>{
+    sortColumn(i);
+});
+const tbody = document.querySelector("table tbody");
+const rows = [
+    ...tbody.querySelectorAll("tr")
+];
+function sortColumn(index) {
+    rows.sort((a, b)=>{
+        const aParams = a.cells[index].textContent;
+        const bParams = b.cells[index].textContent;
+        return aParams.localeCompare(bParams, undefined, {
+            numeric: true
+        });
+    });
+    tbody.innerHTML = "";
+    rows.forEach((row)=>tbody.append(row));
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
